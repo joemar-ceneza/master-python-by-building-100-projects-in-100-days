c0d27ec723f991435d01164d7000e1000e66970a
Joemar Ceneza <joemar.ceneza@gmail.com>
2026-02-22 20:41:15 +0800

    working with variables in python to manage data

---
+++ Day 1/task.py
@@ -0,0 +1,6 @@
+# This line of code will not read
+
+print("Welcome to the Band Name Generator.")
+name_city = input("What's the name of the city you grew up in? \n")
+pet_name = input("What's your pet's name? \n")
+print(f"Your band name could be be: " + name_city + pet_name)
